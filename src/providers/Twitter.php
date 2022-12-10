7ecf39d095569310a078da91903c27ee6a09abfe
Josh Crawford <joshua@sgroup.com.au>
2022-12-10 12:18:20 +1100

    Fix Twitter v2 support

--- src/providers/Twitter.php
+++ src/providers/Twitter.php
@@ -3,6 +3,12 @@ namespace verbb\auth\providers;
 
 use verbb\auth\base\ProviderTrait;
 use verbb\auth\clients\twitter\provider\Twitter as TwitterProvider;
+use verbb\auth\helpers\Session;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 
 class Twitter extends TwitterProvider
 {
@@ -17,6 +23,39 @@ class Twitter extends TwitterProvider
 
     public function getBaseApiUrl(): ?string
     {
-        return 'https://api.twitter.com/1.1/';
+        return 'https://api.twitter.com/2/';
+    }
+
+
+    // Protected Methods
+    // =========================================================================
+
+    protected function getAuthorizationQuery(array $params)
+    {
+        // Store PKCE token
+        Session::set('oauth2verifier', $this->getPkceVerifier());
+
+        return parent::getAuthorizationQuery($params);
+    }
+
+    protected function getAccessTokenRequest(array $params): RequestInterface
+    {
+        // Apply PKCE token
+        $params['code_verifier'] = Session::get('oauth2verifier');
+
+        return parent::getAccessTokenRequest($params);
+    }
+
+    protected function checkResponse(ResponseInterface $response, $data): void
+    {
+        // Twitter often responds with 201, so the current check of `== 200` isn't going to work.
+        $statusCode = $response->getStatusCode();
+
+        if ($statusCode >= 400) {
+            $error = $data['error_description'] ?? '';
+            $code = $data['code'] ?? $statusCode;
+
+            throw new IdentityProviderException($error, $code, $data);
+        }
     }
 }
