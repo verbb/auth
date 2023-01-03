74217e54672f7afb9335b502c3ec8795ed6dfc1d
Josh Crawford <joshua@sgroup.com.au>
2023-01-03 14:31:36 +1100

    Add generic OAuth provider

---
+++ src/providers/Generic.php
@@ -0,0 +1,37 @@
+<?php
+namespace verbb\auth\providers;
+
+use verbb\auth\base\ProviderTrait;
+use verbb\auth\models\Token;
+
+use League\OAuth2\Client\Provider\GenericProvider;
+
+class Generic extends GenericProvider
+{
+    // Traits
+    // =========================================================================
+
+    use ProviderTrait;
+
+
+    // Properties
+    // =========================================================================
+
+    protected ?string $baseApiUrl = null;
+
+
+    // Public Methods
+    // =========================================================================
+
+    public function getBaseApiUrl(): ?string
+    {
+        return $this->baseApiUrl;
+    }
+
+    public function getApiRequestQueryParams(Token $token): array
+    {
+        return [
+            'access_token' => (string)$token->getToken(),
+        ];
+    }
+}
