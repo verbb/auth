f80ecbd46e81ce2e84720a707b2a2feec7591896
Josh Crawford <joshua@sgroup.com.au>
2023-12-02 20:23:03 +1100

    Tweak

--- src/clients/amazon/provider/Amazon.php
+++ src/clients/amazon/provider/Amazon.php
@@ -1,81 +1,42 @@
 <?php
-
 namespace verbb\auth\clients\amazon\provider;
 
 use League\OAuth2\Client\Provider\AbstractProvider;
-use Psr\Http\Message\ResponseInterface;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class Amazon extends AbstractProvider
 {
     use BearerAuthorizationTrait;
 
-    /**
-     * Get authorization url to begin OAuth flow
-     *
-     * @return string
-     */
     public function getBaseAuthorizationUrl(): string
     {
         return 'https://www.amazon.com/ap/oa';
     }
 
-    /**
-     * Get access token url to retrieve token
-     *
-     * @param array $params
-     *
-     * @return string
-     */
     public function getBaseAccessTokenUrl(array $params): string
     {
         return 'https://api.amazon.com/auth/o2/token';
     }
 
-    /**
-     * Get provider url to fetch user details
-     *
-     * @param AccessToken $token
-     *
-     * @return string
-     */
     public function getResourceOwnerDetailsUrl(AccessToken $token): string
     {
         return 'https://api.amazon.com/user/profile';
     }
 
-    /**
-     * Get the default scopes used by this provider.
-     *
-     * @return array
-     */
     protected function getDefaultScopes(): array
     {
         return ['profile'];
     }
 
-    /**
-     * Returns the string that should be used to separate scopes when building
-     * the URL for requesting an access token.
-     *
-     * @return string Scope separator, defaults to ','
-     */
     protected function getScopeSeparator(): string
     {
         return ' ';
     }
 
-    /**
-     * Check a provider response for errors.
-     *
-     * @param  ResponseInterface $response
-     * @param  array|string $data
-     *
-     * @throws IdentityProviderException
-     */
     protected function checkResponse(ResponseInterface $response, $data): void
     {
         if (isset($data['error'])) {
@@ -83,6 +44,7 @@ class Amazon extends AbstractProvider
             $error = $data['error'];
             $errorDescription = $data['error_description'];
             $errorLink = ($data['error_uri'] ?? false);
+            
             throw new IdentityProviderException(
                 $statusCode . ' - ' . $errorDescription . ': ' . $error . ($errorLink ? ' (see: ' . $errorLink . ')' : ''),
                 $response->getStatusCode(),
@@ -91,31 +53,16 @@ class Amazon extends AbstractProvider
         }
     }
 
-    /**
-     * Generate a user object from a successful user details request.
-     *
-     * @param array $response
-     * @param AccessToken $token
-     *
-     * @return AmazonResourceOwner
-     */
     protected function createResourceOwner(array $response, AccessToken $token): AmazonResourceOwner
     {
         return new AmazonResourceOwner($response);
     }
 
-    /**
-     * Returns a prepared request for requesting an access token.
-     *
-     * @param array $params
-     *
-     * @return RequestInterface
-     */
     protected function getAccessTokenRequest(array $params): RequestInterface
     {
         $request = parent::getAccessTokenRequest($params);
-        $uri = $request->getUri()
-            ->withUserInfo($this->clientId, $this->clientSecret);
+        $uri = $request->getUri()->withUserInfo($this->clientId, $this->clientSecret);
+
         return $request->withUri($uri);
     }
 }
