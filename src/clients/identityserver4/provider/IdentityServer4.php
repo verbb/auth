00a7e774409faf09c843831ff35d1a99d4641644
Josh Crawford <joshua@sgroup.com.au>
2024-03-22 15:35:13 +1100

    Fix

--- src/clients/identityserver4/provider/IdentityServer4.php
+++ src/clients/identityserver4/provider/IdentityServer4.php
@@ -1,9 +1,9 @@
 <?php
-namespace verbb\auth\clients\identifyserver4\provider;
+namespace verbb\auth\clients\identityserver4\provider;
 
 use verbb\auth\clients\auth0\provider\Auth0;
 
-class IdentifyServer4 extends Auth0
+class IdentityServer4 extends Auth0
 {
     public function getBaseAccessTokenUrl(array $params = [])
     {
