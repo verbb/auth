15f9fbaa43f9dfc93dae3a43630c8ef43089b814
Josh Crawford <joshua@sgroup.com.au>
2026-07-21 19:38:53 +1000

    Fix `client_credentials` API requests discarding the request payload (empty body), introduced in 2.0.41.

--- src/base/OAuthProviderTrait.php
+++ src/base/OAuthProviderTrait.php
@@ -249,13 +249,13 @@ trait OAuthProviderTrait
         // Check if `client_credentials` grant - a new token should be fetched each time
         // TODO: handle this a little better...
         if ($this->getGrant() === 'client_credentials') {
-            $options = [];
+            $tokenOptions = [];
 
             if (!empty($this->scopes)) {
-                $options['scope'] = $this->scopes;
+                $tokenOptions['scope'] = $this->scopes;
             }
 
-            $accessToken = $oauthProvider->getAccessToken('client_credentials', $this->getAccessTokenOptions($options));
+            $accessToken = $oauthProvider->getAccessToken('client_credentials', $this->getAccessTokenOptions($tokenOptions));
 
             $token = new Token();
             $token->setToken($accessToken);
